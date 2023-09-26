7913c3868e76b5b336b40a2c4791b2b1b401da12
Carlos Alberto Alegre <ca.alegre269@gmail.com>
2023-09-26 18:43:41 -0300

    Añadimos el componente ContactList a la vista principal

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { ContactList } from "./components/ContactList"
 import { NewContactForm } from "./components/NewContactForm"
 
 function App() {
@@ -6,14 +7,22 @@ function App() {
     <>
       <main className='container'>
         <div className='row'>
-          <div className='col-12'>
-            <h1 className='display-1 text-center'>AGENDA</h1>
+          <div className='col-12 col-lg-8 mx-auto'>
+            <h1 className='display-2 text-center'>AGENDA</h1>
             <hr />
           </div>
         </div>
         <div className='row'>
-          <div className='col-12'>
+          <div className='col-12 col-lg-8 mx-auto'>
+            <h2 className="display-5 text-center">Nuevo contacto</h2>
             <NewContactForm />
+            <hr />
+          </div>
+        </div>
+        <div className='row'>
+          <div className='col-12 col-lg-8 mx-auto'>
+            <h2 className="display-5 text-center">Contactos</h2>
+            <ContactList />
             <hr />
           </div>
         </div>
