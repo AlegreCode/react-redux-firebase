e65d2ecd151a5ec6aac37463051b1649ea2571fc
Carlos Alberto Alegre <ca.alegre269@gmail.com>
2023-09-28 19:01:23 -0300

    Establecemos los valores para los input del formulario para modificar los datos de contacto

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,15 +1,48 @@
-export const Contact = ({ contact }) => {
-    return (
-        <tr key={contact.id}>
-            <th scope="row">{contact.id}</th>
-            <td>{contact.firstname}</td>
-            <td>{contact.lastname}</td>
-            <td>{contact.phone}</td>
-            <td>{contact.email}</td>
-            <td>
-                <button className="btn btn-sm btn-success me-2">Editar</button>
-                <button className="btn btn-sm btn-danger">Borrar</button>
-            </td>
-        </tr>
-    )
+import { useRef } from "react"
+import { useState } from "react"
+
+export const Contact = ({ data }) => {
+    const [edit, setEdit] = useState(false)
+
+    const [contact, setContact] = useState({
+        id: data.id,
+        firstname: data.firstname,
+        lastname: data.lastname,
+        email: data.email,
+        phone: data.phone
+    })
+
+    const editHandle = () => {
+        setEdit(!edit)
+    }
+
+    if (!edit) {
+        return (
+            <tr key={data.id}>
+                <th scope="row">{contact.id}</th>
+                <td><input type="text" className="form-control form-control-sm" value={contact.firstname}/></td>
+                <td><input type="text" className="form-control form-control-sm" value={contact.lastname}/></td>
+                <td><input type="text" className="form-control form-control-sm" value={contact.email}/></td>
+                <td><input type="text" className="form-control form-control-sm" value={contact.phone}/></td>
+                <td>
+                    <button className="btn btn-sm btn-success me-2" onClick={editHandle}>EDITAR</button>
+                    <button className="btn btn-sm btn-danger" onClick={editHandle}>BORRAR</button>
+                </td>
+            </tr>
+        )
+    }else{
+        return (
+            <tr key={contact.id}>
+                <th scope="row">{contact.id}</th>
+                <td>{contact.firstname}</td>
+                <td>{contact.lastname}</td>
+                <td>{contact.phone}</td>
+                <td>{contact.email}</td>
+                <td>
+                    <button className="btn btn-sm btn-success me-2" onClick={editHandle}>GUARDAR</button>
+                    <button className="btn btn-sm btn-danger" onClick={editHandle}>CANCELAR</button>
+                </td>
+            </tr>
+        )
+    }
 }
