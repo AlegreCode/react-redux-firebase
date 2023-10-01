9713f61a124d70dfe3e344472d7caa1fd7ced997
Carlos Alberto Alegre <ca.alegre269@gmail.com>
2023-09-30 23:54:52 -0300

    Refactorizamos el código para corregir el error: DOMException: Failed to execute 'removeChild' on 'Node': The node to be removed is not a child of this node.

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -15,6 +15,15 @@ export const Contact = ({ data }) => {
     setEdit(!edit);
   };
 
+  let viewMode = {}
+  let editMode = {}
+  
+  if(edit){
+    viewMode.display = "none";
+  } else {
+    editMode.display = "none";
+  }
+
   const handleInputChange = (e) => {
     const { name, value } = e.target;
     setContact({ ...contact, [name]: value });
@@ -26,75 +35,69 @@ export const Contact = ({ data }) => {
     toggleEdit(); // Cambiar el modo de edición
   };
 
-  const renderContactDetails = () => {
-    return (
-      <tr key={contact.id}>
-        <th scope="row">{contact.id}</th>
-        <td>{contact.firstname}</td>
-        <td>{contact.lastname}</td>
-        <td>{contact.phone}</td>
-        <td>{contact.email}</td>
-        <td>
-          <button className="btn btn-sm btn-success" onClick={toggleEdit}>
-            EDITAR
-          </button>
-          <button className="btn btn-sm btn-danger">BORRAR</button>
-        </td>
-      </tr>
-    );
-  };
+  return (
+      <>
+        <tr style={viewMode}>
+          <th scope="row">{contact.id}</th>
+          <td>{contact.firstname}</td>
+          <td>{contact.lastname}</td>
+          <td>{contact.phone}</td>
+          <td>{contact.email}</td>
+          <td>
+            <button className="btn btn-sm btn-success" onClick={toggleEdit}>
+              EDITAR
+            </button>
+            <button className="btn btn-sm btn-danger">BORRAR</button>
+          </td>
+        </tr>
 
-  const renderEditForm = () => {
-    return (
-      <tr key={contact.id}>
-        <th scope="row">{contact.id}</th>
-        <td>
-          <input
-            type="text"
-            name="firstname"
-            className="form-control form-control-sm"
-            value={contact.firstname}
-            onChange={handleInputChange}
-          />
-        </td>
-        <td>
-          <input
-            type="text"
-            name="lastname"
-            className="form-control form-control-sm"
-            value={contact.lastname}
-            onChange={handleInputChange}
-          />
-        </td>
-        <td>
-          <input
-            type="text"
-            name="email"
-            className="form-control form-control-sm"
-            value={contact.email}
-            onChange={handleInputChange}
-          />
-        </td>
-        <td>
-          <input
-            type="text"
-            name="phone"
-            className="form-control form-control-sm"
-            value={contact.phone}
-            onChange={handleInputChange}
-          />
-        </td>
-        <td>
-          <button className="btn btn-sm btn-danger" onClick={toggleEdit}>
-            CANCELAR
-          </button>
-          <button className="btn btn-sm btn-primary" onClick={handleSave}>
-            GUARDAR
-          </button>
-        </td>
-      </tr>
-    );
-  };
-
-  return edit ? renderEditForm() : renderContactDetails();
+        <tr style={editMode}>
+          <th scope="row">{contact.id}</th>
+          <td>
+            <input
+              type="text"
+              name="firstname"
+              className="form-control form-control-sm"
+              value={contact.firstname}
+              onChange={handleInputChange}
+            />
+          </td>
+          <td>
+            <input
+              type="text"
+              name="lastname"
+              className="form-control form-control-sm"
+              value={contact.lastname}
+              onChange={handleInputChange}
+            />
+          </td>
+          <td>
+            <input
+              type="text"
+              name="email"
+              className="form-control form-control-sm"
+              value={contact.email}
+              onChange={handleInputChange}
+            />
+          </td>
+          <td>
+            <input
+              type="text"
+              name="phone"
+              className="form-control form-control-sm"
+              value={contact.phone}
+              onChange={handleInputChange}
+            />
+          </td>
+          <td>
+            <button className="btn btn-sm btn-danger" onClick={toggleEdit}>
+              CANCELAR
+            </button>
+            <button className="btn btn-sm btn-primary" onClick={handleSave}>
+              GUARDAR
+            </button>
+          </td>
+        </tr>
+      </>
+  );
 };
