d4b147e5d5aaa6b56e8cf8c80560ca3d5fb2578a
Carlos Alberto Alegre <ca.alegre269@gmail.com>
2023-09-30 23:52:53 -0300

    Añadimos los atributos name a cada uno de los campos del formulario del componente NewContactForm.jsx

--- src/components/NewContactForm.jsx
+++ src/components/NewContactForm.jsx
@@ -5,18 +5,18 @@ export const NewContactForm = () => {
             <form action="">
                 <div className="row mb-3">
                     <div className="col">
-                        <input type="text" className="form-control" placeholder="First name" aria-label="First name"/>
+                        <input type="text" name="firstname" className="form-control" placeholder="First name" aria-label="First name"/>
                     </div>
                     <div className="col">
-                        <input type="text" className="form-control" placeholder="Last name" aria-label="Last name"/>
+                        <input type="text" name="lastname" className="form-control" placeholder="Last name" aria-label="Last name"/>
                     </div>
                 </div>
                 <div className="row mb-3">
                     <div className="col">
-                        <input type="text" className="form-control" placeholder="Phone number" aria-label="Phone number"/>
+                        <input type="text" name="phone" className="form-control" placeholder="Phone number" aria-label="Phone number"/>
                     </div>
                     <div className="col">
-                        <input type="text" className="form-control" placeholder="E-mail" aria-label="E-mail"/>
+                        <input type="text" name="email" className="form-control" placeholder="E-mail" aria-label="E-mail"/>
                     </div>
                 </div>
                 <div className="row">
