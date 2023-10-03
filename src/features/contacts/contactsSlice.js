3f50eac4a6acbe5ca6a9860e0684cc12d10455dd
Carlos Alberto Alegre <ca.alegre269@gmail.com>
2023-10-03 18:49:59 -0300

    Añadimos el reducer AddContact, el cual añade un nuevo contacto al store

--- src/features/contacts/contactsSlice.js
+++ src/features/contacts/contactsSlice.js
@@ -7,10 +7,27 @@ const initialState = contactsAdapter.getInitialState()
 export const contactsSlice = createSlice({
     name: "contacts",
     initialState,
-    reducers: {},
+    reducers: {
+        addContact: {
+            reducer: contactsAdapter.addOne,
+            prepare(firstname, lastname, phone, email){
+                return {
+                    payload: {
+                        id: Date.now(),
+                        firstname,
+                        lastname,
+                        phone,
+                        email,
+                    }
+                }
+            }
+        }
+    },
 })
 
 export default contactsSlice.reducer
+
+export const { addContact } = contactsSlice.actions
 
 export const { selectAll: selectContacts } = contactsAdapter.getSelectors(
     (state) => state.contacts
